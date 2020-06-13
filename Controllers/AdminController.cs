323a1430933fb7b81d7efb2eafc50790dcb26336
kosimovanodira <nkosimova06@gmail.com>
2020-06-13 22:02:58 +0500

    add StudentList with actions

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -23,15 +23,6 @@ namespace Project.Controllers
         {            
             return View(admin);
         }
-
-        public IActionResult StudentList()
-        {            
-            return View(data.Students.ToList());
-        }
-        public IActionResult TeacherList()
-        {
-            return View(data.Teachers.ToList());
-        }
         public IActionResult FacultyList()
         {
             return View(data.Faculties.ToList());
@@ -39,6 +30,11 @@ namespace Project.Controllers
         public IActionResult SubjectLIst()
         {
             return View(data.Subjects.ToList());
+        }
+        //Методы для работы с данными таблицы преподавателей
+        public IActionResult TeacherList()
+        {
+            return View(data.Teachers.ToList());
         }
         [HttpGet]
         public IActionResult DeleteTeacher(int id)
@@ -48,14 +44,13 @@ namespace Project.Controllers
             data.SaveChanges();
             return RedirectToAction("TeacherList", data.Teachers.ToList());            
         }
-      //  [HttpGet]
         public IActionResult ChangeTeacher(int Id)
         {
             Teacher teacher = data.Teachers.Find(Id);
             if (teacher != null)
-                return PartialView("ChangeTeacher",teacher);
+                return View("ChangeTeacher",teacher);
             return RedirectToAction("TeacherList", data.Teachers.ToList());             
-        }
+        }        
         [HttpPost]
         public IActionResult ChangeTeacher(Teacher teacher)
         {
@@ -68,8 +63,57 @@ namespace Project.Controllers
             }
             return RedirectToAction("TeacherList", data.Teachers.ToList());
         }
-
-
+        //Методы для работы с данными таблицы студентов
+        public IActionResult StudentList()
+        {   
+            StudCourseFaculSubj model = new
+            StudCourseFaculSubj{
+                Courses = data.Courses,
+                Students = data.Students,
+                Faculties = data.Faculties,
+                Subjects = data.Subjects
+            };
+            return View(model);
+        }
+        public IActionResult ChangeStudent(int Id)
+        {
+            Student student = data.Students.Find(Id);
+            if (student != null)
+            {
+                CourseFacultySubject model = new
+                CourseFacultySubject{
+                    Courses = data.Courses,
+                    Faculties = data.Faculties,
+                    student = student
+                };
+                return View(model);
+            }
+            return RedirectToAction("StudentList", data.Students.ToList());
+        }            
+        [HttpPost]
+        public IActionResult ChangeStudent(Student student)
+        {
+            if (data.Students.Find(student.StudentId) != null)
+            {
+                data.Students.Find(student.StudentId).StudentFirstName = student.StudentFirstName;
+                data.Students.Find(student.StudentId).StudentSecondName = student.StudentSecondName;
+                data.Students.Find(student.StudentId).StudentMiddleName = student.StudentMiddleName;
+                data.Students.Find(student.StudentId).IdCourse = student.IdCourse;
+                data.Students.Find(student.StudentId).IdFaculty = student.IdFaculty;
+                data.SaveChanges();
+            }
+            return RedirectToAction("StudentList", data.Students.ToList());
+        }
+        [HttpGet]
+        public IActionResult DeleteStudent(int Id)
+        {
+            Student student = data.Students.Find(Id);
+            data.Students.Remove(student);
+            data.SaveChanges();
+            return RedirectToAction("StudentList", data.Teachers.ToList());            
+        }
+        //Методы для работы с данными таблицы предметов
+        //Методы для работы с данными таблицы факультетов
 
     }
 }
