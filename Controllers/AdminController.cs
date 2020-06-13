10079b05711f92763eaab7ce17c978695a665dca
kosimovanodira <nkosimova06@gmail.com>
2020-06-13 20:13:04 +0500

    add TeacherList with actions

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -21,7 +21,55 @@ namespace Project.Controllers
         [HttpGet]
         public IActionResult Index(Admin admin)
         {            
-            return View();
+            return View(admin);
         }
+
+        public IActionResult StudentList()
+        {            
+            return View(data.Students.ToList());
+        }
+        public IActionResult TeacherList()
+        {
+            return View(data.Teachers.ToList());
+        }
+        public IActionResult FacultyList()
+        {
+            return View(data.Faculties.ToList());
+        }
+        public IActionResult SubjectLIst()
+        {
+            return View(data.Subjects.ToList());
+        }
+        [HttpGet]
+        public IActionResult DeleteTeacher(int id)
+        {
+            Teacher teacher = data.Teachers.FirstOrDefault(i => i.TeacherId == id); 
+            data.Teachers.Remove(teacher);
+            data.SaveChanges();
+            return RedirectToAction("TeacherList", data.Teachers.ToList());            
+        }
+      //  [HttpGet]
+        public IActionResult ChangeTeacher(int Id)
+        {
+            Teacher teacher = data.Teachers.Find(Id);
+            if (teacher != null)
+                return PartialView("ChangeTeacher",teacher);
+            return RedirectToAction("TeacherList", data.Teachers.ToList());             
+        }
+        [HttpPost]
+        public IActionResult ChangeTeacher(Teacher teacher)
+        {
+            if (data.Teachers.Find(teacher.TeacherId) != null)
+            {            
+                data.Teachers.Find(teacher.TeacherId).TeacherFirstName = teacher.TeacherFirstName;
+                data.Teachers.Find(teacher.TeacherId).TeacherSecondName = teacher.TeacherSecondName;
+                data.Teachers.Find(teacher.TeacherId).TeacherMiddleName = teacher.TeacherMiddleName;
+                data.SaveChanges();
+            }
+            return RedirectToAction("TeacherList", data.Teachers.ToList());
+        }
+
+
+
     }
 }
