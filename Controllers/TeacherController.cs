a0e3ac645db2c1f59842a57ce128a982c8670797
kosimovanodira <nkosimova@gmail.com>
2020-06-15 13:38:06 +0500

    add TestList without actions

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -28,6 +28,8 @@ namespace Project.Controllers
                 return View(data.Teachers.Find(TeacherId));
             return RedirectToAction("Error", new{error = "Пользователь не найден"});
         }
+
+        //создание нового теста
         [HttpGet]
         public IActionResult CreateTest(int TeacherId)
         {
@@ -60,17 +62,85 @@ namespace Project.Controllers
         [HttpPost]
         public IActionResult ChooseSubj(Test test)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                data.Tests.Add(test);
-                data.SaveChanges();
-                return RedirectToAction("Error", new{error = "Тест добавлен"}); 
+                return RedirectToAction("Error", new{error = "Что-то пошло не так"});
             }
-            return RedirectToAction("Error", new{error = "Что-то пошло не так"});
+            if (data.Tests.FirstOrDefault(i => i.TestName == test.TestName) != null)
+            {
+                ModelState.AddModelError("TestName", "Тест с таким названием уже есть. Выберите другое!");               
+                Subject subj = data.Subjects.Find(test.IdSubject);
+                CourseFacultySubject newmodel = new
+                CourseFacultySubject{
+                    Subjects = data.Subjects.Where(i=> i.IdCourse == subj.IdCourse && i.IdFaculty==subj.IdFaculty),
+                    test = new Test{
+                        IdTeacher = test.IdTeacher
+                    }
+                };
+                return View(newmodel);
+            }
+            data.Tests.Add(test);
+            data.SaveChanges();
+            return RedirectToAction("SuccesMessage", new{TeacherId = test.IdTeacher}); 
         }
-        public IActionResult TestList()
+        public IActionResult SuccesMessage(int TeacherId)
         {
-            return View();
+            return View(data.Teachers.Find(TeacherId));
+        }
+        //редактирование тестов, добавление, изменение, удаление вопросов
+        public IActionResult TestList(int TeacherId)
+        {
+            if (TeacherId != 1) return RedirectToAction("Error", new{error = "NO ID"});
+            Course allCourse = new Course{CourseId = 0, CourseName = "Все курсы"};
+            Faculty allFaculty = new Faculty{FacultyId = 0, FacultyName = "Все факультеты"};
+            List<Course> courses = data.Courses.ToList();
+            List<Faculty> faculties = data.Faculties.ToList();
+            courses.Insert(0, allCourse);
+            faculties.Insert(0, allFaculty);
+            
+           TestHelperModel model = new TestHelperModel{
+                Courses = courses, 
+                Faculties = faculties,
+                teacher = new Teacher{TeacherId = TeacherId}  };
+                model.Subjects = data.Subjects.ToList();
+                model.Tests = data.Tests.Where(i => i.IdTeacher == TeacherId)
+                                        .OrderBy(i => i.IdSubject).ToList();
+                return View(model);
+        }
+        [HttpPost]
+        public IActionResult TestList(int Idcourse, int Idfaculty, int Idteacher)
+        {
+            if (Idteacher != 1) return RedirectToAction("Error", new{error = "NO ID"});
+            Course allCourse = new Course{CourseId = 0, CourseName = "Все курсы"};
+            Faculty allFaculty = new Faculty{FacultyId = 0, FacultyName = "Все факультеты"};
+            List<Course> courses = data.Courses.ToList();
+            List<Faculty> faculties = data.Faculties.ToList();
+            courses.Insert(0, allCourse);
+            faculties.Insert(0, allFaculty);
+            
+           TestHelperModel model = new TestHelperModel{
+               Courses = courses, 
+               Faculties = faculties,
+               teacher = new Teacher{TeacherId = Idteacher}  };
+            if (Idcourse == 0)
+            {
+                model.Subjects = data.Subjects.ToList();
+                model.Tests = data.Tests.Where(i => i.IdTeacher == Idteacher)
+                                        .OrderBy(i => i.IdSubject).ToList();
+            }
+            else
+            {
+                model.Subjects = data.Subjects.Where(i => i.IdCourse == Idcourse).ToList();
+                model.Tests = data.Tests.Where(i => data.Subjects.Find(i.IdSubject).IdCourse == Idcourse)
+                                        .OrderBy(i => i.IdSubject).ToList();
+            }
+            if (Idfaculty > 0)
+            {
+                model.Subjects = model.Subjects.Where(i => i.IdFaculty == Idfaculty).ToList();
+                model.Tests = model.Tests.Where(i => data.Subjects.Find(i.IdSubject).IdFaculty == Idfaculty)
+                                        .OrderBy(i => i.IdSubject).ToList();
+            }
+            return View(model);
         }
         public string Error(string error)
         {
--- Models/HelpModels/TestCreateModel.cs
+++ Models/HelpModels/TestCreateModel.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 namespace TestSystem.Models
 {
-    public class TestCreateModel
+    public class TestHelperModel
     {
         public IEnumerable<Course> Courses{get; set;}
         public IEnumerable<Faculty> Faculties{get; set;}
         public IEnumerable<Subject> Subjects{get; set;}
-        public Student student{get; set;}
-        public Subject subject{get; set;}
+        public IEnumerable<Test> Tests{get; set;}
         public Test test{get; set;}
+        public Teacher teacher{get; set;}
 
     }
 }
